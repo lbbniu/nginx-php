50d8e80ebfbdd4be61a38ffaf91f358e868ff0b0
lbbniu <lbbniu@qq.com>
2016-08-16 10:36:49 +0800

    修改部署脚本,修改Dockerfile文件中的路径错误

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 
-FROM lbbniu/bignginxserver
+FROM lbbniu/nginxphpserver
 
 MAINTAINER lbbniu lbbniu@gmail.com
 
@@ -8,21 +8,21 @@ COPY ./run.sh /lbbniu/run.sh
 #nginx 配置文件
 COPY ./nginx.conf /lbbniu/nginx/conf/nginx.conf
 #php 配置文件
-COPY ./php.ini /lbbniu/php/etc/php.ini
+COPY ./php.ini /lbbniu/php7/etc/php.ini
 #php-fpm 配置文件
-COPY ./php-fpm.conf /lbbniu/php/etc/php-fpm.conf
+COPY ./php-fpm.conf /lbbniu/php7/etc/php-fpm.conf
 #php-fpm 配置文件
-COPY ./www.conf /lbbniu/php/etc/php-fpm.d/www.conf
+COPY ./www.conf /lbbniu/php7/etc/php-fpm.d/www.conf
 
 #nginx虚拟主机配置文件目录  日志目录  web主目录
 VOLUME ["/lbbniu/nginx/conf/vhost","/lbbniu/logs","/lbbniu/wwwroot"]
 
 #这里可以加入证书文件，实现ssh登录
 
-RUN chmod 755 /lbbniu/run.sh &&\
-	rm -rf /var/lib/apt/lists/* &&\
-	chmod 777 /lbbniu/logs
-
+RUN chmod 755 /lbbniu/run.sh && \
+	chmod 777 /lbbniu/logs && \
+    echo "Asia/Shanghai" > /etc/timezone
+# 22 80
 EXPOSE 80
 
 CMD ["/lbbniu/run.sh"]
--- dc-prod.sh
+++ dc-prod.sh
@@ -5,15 +5,29 @@ export SERVERPATH=/lbbniu/wwwroot
 #容器名字
 export NAME=nginx-php
 #镜像
-export DC_IMAGES=lbbniu/nginx-php
+export DC_IMAGE=lbbniu/nginx-php
+export LOCAL_IMAGE=lbbniu/nginxphpserver
+
+export filename=nginxphp.tar
+if [ ! -e $filename ];then
+    wget http://cdn.vliang.com/nginxphp.tar.gz
+    tar zxf nginxphp.tar.gz
+    echo "解压完成....."
+fi
+docker rmi $LOCAL_IMAGE
+echo $LOCAL_IMAGE "镜像删除成功...."
+cat $filename | docker import - $LOCAL_IMAGE
+echo $LOCAL_IMAGE "镜像导入成功...."
+
 
 docker stop $NAME
-echo $NAME "stop......."
+echo $NAME "容器停止......."
 docker rm -f $NAME
-echo $NAME "rm......."
-#docker rmi -f $DC_IMAGES
+echo $NAME "容器删除......."
 
-docker build -t $DC_IMAGES .
-
-docker run -d -p 80:80 -v $WEBPATH:$SERVERPATH --name $NAME $DC_IMAGES
-echo $NAME "is run ....."
+docker rmi -f $DC_IMAGE
+echo $DC_IMAGE "镜像删除......."
+docker build -t $DC_IMAGE .
+echo $DC_IMAGE "镜像构建成功......."
+docker run -d -p 80:80 -v $WEBPATH:$SERVERPATH --name $NAME $DC_IMAGE
+echo $NAME "启动成功 ....."
